a1f4f638e50b48ff2cdb88c7c277dea6f2671e81
najeeb678 <101343669+najeeb678@users.noreply.github.com>
2024-02-20 11:37:28 +0500

    project-setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './components/home'
+import LoginForm from './components/LoginForm'
+import SignUp from './components/signUp'
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <LoginForm />
+      <SignUp/>
+      
+    </div>
+  )
+}
+
+export default App
+
