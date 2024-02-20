a9b71336ff7a377645edd657f2922066614080ab
najeeb678 <101343669+najeeb678@users.noreply.github.com>
2024-02-20 12:23:53 +0500

    new feature

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const LoginForm = () => {
   return (
     <div>
       <h1>LoginForm</h1>
+      jdncjkanscklmqasklcmaklm
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
